de6fca90498a2881c831dd51cb4b221a07613088
stephenkati <stephenkatuli88@gmail.com>
2023-07-17 18:18:51 +0300

    fix errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
     greet = @greetings.sample
-    
+
     sleep 1
     render json: greet
   end
